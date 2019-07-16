f452a91ea6ecef62efe9e4fee8901963fbfe0803
Ankit kumrawat <Ankit.kumrawat@ril.com>
2019-07-16 19:25:22 +0530

    Added changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient
+let db=null;
+MongoClient.connect('mongodb+srv://admin:admin@cluster0-cryvl.mongodb.net/test?retryWrites=true&w=majority', function (err, client) {
+  if (err) throw err
 
+  else console.log('111111111111111111');
+  db = client.db('pocker');
+
+
+})
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.collection('score').find().toArray(function (err, result) {
+    if (err) throw err;
+    else{
+      console.log(result[0].score)
+      res.render('index');
+    }
+  })
 });
 
 module.exports = router;
@@ -12,20 +27,7 @@ module.exports = router;
 // mongodb+srv://admin:admin@cluster0-cryvl.mongodb.net/test?retryWrites=true&w=majority
 
 
-/*
-var MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb+srv://admin:admin@cluster0-cryvl.mongodb.net/test?retryWrites=true&w=majority', function (err, client) {
-  if (err) throw err
 
-  else console.log('111111111111111111');
-  var db = client.db('sample_geospatial')
 
-  db.collection('shipwrecks').find().toArray(function (err, result) {
-    if (err) throw err
-
-    console.log(result)
-  })
-})
-*/
 
